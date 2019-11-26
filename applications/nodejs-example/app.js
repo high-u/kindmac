388ef02490d1408893834ab341dba5d748ce1f85
hig <yhiguchi@uhuru.jp>
2019-11-27 00:38:09 +0900

    update nodejs-example

--- applications/nodejs-example/app.js
+++ applications/nodejs-example/app.js
@@ -1,17 +1,5 @@
-// https://nodejs.org/en/docs/guides/nodejs-docker-webapp/
-'use strict';
-
-const express = require('express');
-
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
-});
-
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+'use strict'
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(3000, () => console.log(`Example app listening on port 3000 !`))
