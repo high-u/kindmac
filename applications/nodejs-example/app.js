a51361743b3282da06b67dbe3c107d9b5de51be7
hig <yhiguchi@uhuru.jp>
2019-11-29 22:35:03 +0900

    add kafka-visualizer and update nodejs-example

--- applications/nodejs-example/app.js
+++ applications/nodejs-example/app.js
@@ -1,5 +1,17 @@
 'use strict'
 const express = require('express')
+const axios = require('axios')
+
 const app = express()
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  axios.get('https://www.google.com/')
+    .then((response) => {
+      res.send(`${response.status}`)
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+    .finally(function () {
+    })
+})
 app.listen(3000, () => console.log(`Example app listening on port 3000 !`))
